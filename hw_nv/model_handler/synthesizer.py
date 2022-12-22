fix normalization, add normal init

hw_nv/model_handler/synthesizer.py
@@ -5,7 +5,7 @@ from torch.nn.utils import remove_weight_norm
 import torch.nn.functional as F
 from torchvision.utils import make_grid
 
-from .utils import remove_norm
+from .utils import remove_norm, normal_init
 from .. import model
 from ..dataset import MelSpectrogram
 from ..logger import WanDBWriter
@@ -26,12 +26,13 @@ class NVSynthesizer():
         self.generator = model.Generator(config["architecture"])
         if checkpoint_path is not None:
             self.generator.load_state_dict(torch.load(checkpoint_path)["architecture"])
-        
-        # remove weight norm if it is in place
-        try:
-            remove_norm(self.generator, remove_weight_norm)
-        except Exception: 
-            pass
+            # remove weight norm if it is in place
+            try:
+                remove_norm(self.generator, remove_weight_norm)
+            except Exception: 
+                pass
+        else:
+            normal_init(self.generator)
     
         self.generator = self.generator.to(self.device)
         

hw_nv/model_handler/utils.py
@@ -6,7 +6,8 @@ def apply_norm(model, norm_fn):
         Applies norm_fn to all suitable model layers
     """
     for param in model.parameters():
-        if isinstance(param, nn.Conv2d):
+        if isinstance(param, nn.Conv2d) or isinstance(param, nn.Conv1d):
+            print("!")
             norm_fn(param)
     return model
     
@@ -15,7 +16,16 @@ def remove_norm(model, remove_norm_fn):
     """
         Removes norm from all suitable model layers
     """
-    for param in model.named_parameters():
-        if isinstance(param, nn.Conv2d):
+    for param in model.parameters():
+        if isinstance(param, nn.Conv2d) or isinstance(param, nn.Conv1d):
             remove_norm_fn(param)
+    return model
+
+def normal_init(model):
+    """
+        Initialise model weghts with N(0, 0.01)
+    """
+    for param in model.parameters():
+        if isinstance(param, nn.Conv2d) or isinstance(param, nn.Conv1d):
+            param.weight.data.normal_(0, 0.01)
     return model